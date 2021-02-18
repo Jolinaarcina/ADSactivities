e5172be126402c0c93e592d8c2e3409fedcf9e7f
Jolina Arcina <jolinaarcina2@gmail.com>
2021-02-19 01:35:57 +0800

    added actvity 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//POST-TEST (laboratory-module 3) 
+//1.
+Route::get('/addition', function () {
+    	 $sum = 5+8;
+    return ("The sum of 5 and 8 is $sum.");
+});
+Route::get('/multiply', function () {
+    	 $diff= 10-1;
+    	 return("The difference of 10 and 1 is $diff."); 
+});
+Route::get('/product', function () {
+          $prod= 3*2;
+          return ("The product of 3 and 2 is $prod.");
+});
+Route::get('/quotient', function () {
+    	  $quotient= 10/2;
+    	  return ("The quotient of 10 and 2 is $quotient.");
+});
+Route::get('/numbers', function () {
+    	$collectionA = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+		$collectionB = $collectionA->collect();
+   		return $collectionB->all(); 
+});
